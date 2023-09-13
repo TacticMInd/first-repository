1c0af6a0368c91517d9eed40ef696088db6850e7
TacticMInd <144225392+TacticMInd@users.noreply.github.com>
2023-09-13 15:38:40 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,11 @@
-def print_hello():
-    print("GoodBye World!!")
+def add(x, y):
+    a = x+ y 
+    return a
 
-def print_now():
-    print("I did all my work")
+def print_KIT():
+    print("KIT")
 
 if __name__ == '__main__':
-    print_hello()
+    print_KIT()
+    print( print_hello(1, 2) )
     
